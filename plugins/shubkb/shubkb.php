43e243b9fabb4016223597a2b87e9666abb27dc2
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2024-01-20 15:03:03 +0530

    Add: Standard files and start a plugin

---
+++ plugins/shubkb/shubkb.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Shubkb Plugin
+ * Plugin URI: https://shubkb.com
+ * Description: This is a plugin for shubkb.com
+ * Version: 1.0.0
+ * Author: Shubham Kumar Bansal
+ * Author URI: https://shubkb.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: shubkb
+ * Domain Path: /languages
+ * PHP version 7.2
+ *
+ * @category Plugin
+ * @package  Shubkb_Plugin
+ * @author   shubham <shub@shubkb.com>
+ * @license  GPL2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link     https://shubkb.com
+ */
