2c794e718745734e970c0ab3a718e915863a8b1e
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2024-01-24 02:08:35 +0530

    Fix: PHPCS

--- themes/papersync/functions.php
+++ themes/papersync/functions.php
@@ -1,5 +1,4 @@
 <?php
-use WpOrg\Requests\Auth\Basic;
 /**
  * Functions and definitions
  *
---
+++ themes/papersync/inc/blocks/course-index/block.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Course Index
+ *
+ * A block to display a list of courses and Edit Options.
+ *
+ * @package PaperSync
+ * @since 1.0.0
+ */
