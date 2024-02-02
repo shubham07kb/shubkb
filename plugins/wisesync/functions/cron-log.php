24a2dfbdc401596f3378073feda2c6dd47371265
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2024-02-02 20:33:22 +0530

    Update: Cron Log time format

--- plugins/wisesync/functions/cron-log.php
+++ plugins/wisesync/functions/cron-log.php
@@ -16,12 +16,12 @@ function wisesync_log_cron() {
 	require_once ABSPATH . '/wp-admin/includes/file.php';
 	WP_Filesystem();
 	// Get the current time.
-	$current_time = current_time( 'mysql' );
+	$current_time = gmdate( 'Y/m/d l H:i:s e' );
 	$log_path     = WP_CONTENT_DIR . '/uploads/cron_' . $current_year . '_' . $current_month . '.log';
 	if ( ! $wp_filesystem->exists( $log_path ) ) {
 		$wp_filesystem->touch( $log_path );
 		$wp_filesystem->put_contents( $log_path, 'Cron Log File' . PHP_EOL, FS_CHMOD_FILE );
 	}
 	$pre_cron_log = $wp_filesystem->get_contents( $log_path );
-	$wp_filesystem->put_contents( $log_path, $pre_cron_log . $current_time . ' : Run' . PHP_EOL, FS_CHMOD_FILE );
+	$wp_filesystem->put_contents( $log_path, $pre_cron_log . 'Cron: ' . $current_time . PHP_EOL, FS_CHMOD_FILE );
 }
